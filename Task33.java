200a630eec336e4c2404f05802b1ccb933ee0b1d
FAYEZKASSEM0 <67088058+FAYEZKASSEM0@users.noreply.github.com>
2020-06-23 21:33:53 +0200

    Update Task33.java

--- Task33.java
+++ Task33.java
@@ -3,6 +3,15 @@ package task.pkg33;
 public class Task33 {
 
     public static void main(String[] args) {
+       /* PRINT
+          *
+         ***
+        *****
+       *******
+      *********
+         */
+        
+        
         for (int spc = 4, ast = 1; spc >= 0; spc--, ast += 2) {
             for (int i = 0; i < spc; i++) {
                 System.out.print(" ");
