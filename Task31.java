b699db17d5302ca434b223b6bc908972b5d74a40
FAYEZKASSEM0 <67088058+FAYEZKASSEM0@users.noreply.github.com>
2020-06-23 21:31:16 +0200

    Update Task31.java

--- Task31.java
+++ Task31.java
@@ -5,6 +5,13 @@ public class Task31 {
    
     public static void main(String[] args) {
 
+       /* PRINT 
+       1
+       12
+       123
+       1234
+       12345
+       */
         for (int i = 0; i <= 5; i += 1) {
             for (int j = 0; j < i; j++) {
                 System.out.print(j + 1);
