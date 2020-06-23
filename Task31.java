6a9c25c34d3bb7bd3353cb125ec3b10ee7ccf4b4
FAYEZKASSEM0 <67088058+FAYEZKASSEM0@users.noreply.github.com>
2020-06-23 21:27:46 +0200

    Update Task31.java

--- Task31.java
+++ Task31.java
@@ -2,6 +2,7 @@ package task31;
 
 public class Task31 {
 
+    //write a program take a number from user “ whatever number of digits “ and then display summation of digits and stop where summation is one digit
     public static void main(String[] args) {
 
         for (int i = 0; i <= 5; i += 1) {
